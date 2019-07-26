b9eb0dd02dc0d9b6f7d4cd22ed7dc5c0e87e9cd7
behemoth <LuisScheurenbrand@gmail.com>
2019-07-26 21:57:06 +0200

    create config file if it doesn't exist

--- source/main.cpp
+++ source/main.cpp
@@ -16,19 +16,8 @@ int main(int argc, char **argv)
         menuMainLoop();
     else
     {
-        printf("Failed to open Sys-clk Config! press + to exit.");
-        while (appletMainLoop())
-        {
-            //Scan all the inputs. This should be done once for each frame
-            hidScanInput();
-            //hidKeysDown returns information about which buttons have been just pressed (and they weren't in the previous frame)
-            u64 kDown = hidKeysDown(CONTROLLER_P1_AUTO);
-
-            if (kDown & KEY_PLUS)
-                break;
-
-            consoleUpdate(nullptr);
-        }
+        fclose(fopen(configFile.c_str(), "w"));
+        menuMainLoop();
     }
     consoleExit(nullptr);
     return 0;
