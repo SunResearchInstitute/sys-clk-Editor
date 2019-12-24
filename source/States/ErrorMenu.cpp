07388bff143d19d98dd4f963f159a0e1c9666926
cathery <dumdumcathery@gmail.com>
2019-12-24 14:02:06 +0300

    Fix error screen message

--- source/States/ErrorMenu.cpp
+++ source/States/ErrorMenu.cpp
@@ -6,9 +6,7 @@ Result ErrorMenu::error = 0;
 
 void ErrorMenu::Update(StateMachine & stateMachine, u64 kDown)
 {
-    char str[35];
     consoleClear();
-    sprintf(str, "Error: 0x%x", ErrorMenu::error);
-    printf(CONSOLE_RED "\x1b[21;%d%s", center(80, (int)strlen(str)), str);
-    printf(CONSOLE_RED "\x1b[22;%dPress `+` to exit!", center(80, 17));
+    printf(CONSOLE_RED "Error: 0x%x\n", ErrorMenu::error);
+    printf(CONSOLE_RED "Press `+` to exit!\n");
 }
